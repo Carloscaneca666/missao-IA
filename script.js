80f50723f267ff257f918a1fdf55fd7c35e4f5c6
Carloscaneca666 <127507864+Carloscaneca666@users.noreply.github.com>
2024-07-04 10:57:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+
 const perguntas =[
     {
         enunciado: "Pergunta 1",
         alternativas:["Alternativa 1", "Alternativa 2"],
+    
     },
     {
         enunciado: "Pergunta 2",
@@ -16,6 +18,7 @@ const perguntas =[
         enunciado: "Pergunta 3",
         alternativas:["Alternativa 1", "Alternativa 2"],
     },
+    
 ];
 
 let atual = 0;
@@ -27,10 +30,10 @@ function mostraPerguntas(){
     mostraAlternativas();
 }
 function mostraAlternativas(){
-    for (const alternativa of perguntaAtual.alternativas){
-        const botaoAlternativas = document.createElementNS("button");
-        botaoAlternativas.textContent = alternativa;
-        caixaAlternativas.appendChild(botaoAlternativas);
-    }
+   for (const alternativa of perguntaAtual.alternativas){
+    const botaoAlternativas = document.createElement("button");
+    botaoAlternativas.textContent = alternativa;
+    caixaAlternativas.appendChild(botaoAlternativas);
+   } 
 }
 mostraPerguntas();
